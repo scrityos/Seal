2fa9310d5cb22085abdab0a1dba1a96da54fe892
scrityos <pixel.scrityos@gmail.com>
2024-12-04 18:54:21 +0530

    Update UpdateUtil.kt

--- app/src/main/java/com/junkfood/seal/util/UpdateUtil.kt
+++ app/src/main/java/com/junkfood/seal/util/UpdateUtil.kt
@@ -31,8 +31,8 @@ import okhttp3.ResponseBody
 
 object UpdateUtil {
 
-    private const val OWNER = "JunkFood02"
-    private const val REPO = "Seal"
+    private const val OWNER = "abhiram79"
+    private const val REPO = "Getit"
     private const val ARM64 = "arm64-v8a"
     private const val ARM32 = "armeabi-v7a"
     private const val X86 = "x86"
